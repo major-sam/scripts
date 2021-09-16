afdb7b7a9ac4691257b0fa20c4c30d7ba3245741
enesudimov <enesudimov@baltbet.ru>
2021-09-16 17:45:28 +0300

    Add Generate-DeployScript .ps1

---
+++ ps1/Generate-DeployScript.ps1
@@ -0,0 +1,72 @@
+Param (
+    [Parameter(Mandatory=$true)]
+    [string]$ServiceName,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("Site","Service")]
+    [string]$ServiceType
+)
+
+$GeneratedScriptFileName = "Deploy-$ServiceName.ps1"
+switch ($ServiceType)
+{
+    "Site" {$PathToParentFolder = "C:\inetpub"}
+    "Service" {$PathToParentFolder = "C:\Services"}
+}
+
+
+$TemplateScriptContent = 
+"<#
+    $ServiceName
+    <Здесь должно быть ваше описание скрипта>
+#>
+Param (
+    [Parameter(Mandatory=`$true)]
+    [ValidateSet('TEST','STAGE','PROD')]
+    [string]`$Environment = 'TEST'
+)
+
+
+function Deploy-$ServiceName
+{
+    param ()
+    `$ServiceName = '$ServiceName'
+    `$ServiceFolderPath = '$PathToParentFolder\$ServiceName'
+
+    # Удаление старой конфигурации
+    # <--- писать тут --->
+    # 
+
+
+    # Редактирование конфигов
+    # <--- писать тут --->
+    # 
+
+
+    # Настройка сервиса
+    # <--- писать тут --->
+    # 
+
+}
+
+
+# Определение переменных в зависимости от выбранного окружения
+switch (`$Environment)
+{
+    'PROD' {Write-Host 'Not complited. Run TEST env';exit}
+    'STAGE' {Write-Host 'Not complited. Run TEST env';exit}
+    'TEST' {Write-Host 'Not complited. Run TEST env';exit}
+}
+
+
+# Загрузка доп модулей
+# <--- писать тут --->
+# 
+
+Deploy-$ServiceName # Парметры функции
+"
+
+
+New-Item -Path "C:\Users\$env:USERNAME\Desktop\$GeneratedScriptFileName" -ItemType File -Value $TemplateScriptContent
+$ScriptFullPath = (Get-Item -Path "C:\Users\$env:USERNAME\Desktop\$GeneratedScriptFileName").FullName
+Write-Host -ForegroundColor Green "Создан новый скрипт: $ScriptFullPath"
